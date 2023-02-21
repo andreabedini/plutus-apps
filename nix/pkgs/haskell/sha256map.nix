4260f67497d925bcdfcda5167e776f3d399847d7
Ziyang Liu <unsafeFixIO@gmail.com>
2023-02-21 14:42:18 -0800

    Dump job for 1.35.4

--- nix/pkgs/haskell/sha256map.nix
+++ nix/pkgs/haskell/sha256map.nix
@@ -3,8 +3,9 @@
   "https://github.com/input-output-hk/cardano-config"."1646e9167fab36c0bff82317743b96efa2d3adaa" = "sha256-TNbpnR7llUgBN2WY7CryMxNVupBIUH01h1hRNHoxboY=";
   "https://github.com/input-output-hk/cardano-ledger"."da3e9ae10cf9ef0b805a046c84745f06643583c2" = "sha256-3VUZKkLu1R43GUk9IwgsGQ55O0rnu8NrCkFX9gqA4ck=";
   "https://github.com/input-output-hk/cardano-wallet"."18a931648550246695c790578d4a55ee2f10463e" = "0i40hp1mdbljjcj4pn3n6zahblkb2jmpm8l4wnb36bya1pzf66fx";
+  "https://github.com/input-output-hk/plutus"."ba5b96ec980f2e9902b8669d2f0e0ea556bb7413" = "sha256-ORZCuLaLcU0b+dUnHNJoxcC9thfDZ+Yic/JgLW7Pvwg=";
   "https://github.com/Quviq/quickcheck-contractmodel"."b66e3ff0c6011dcef63f2bcfa6ea8d6ab685230d" = "sha256-gnY/Ps9i7RqO3yjcFilXjHyqDsdSZ+sLQn4vu3mAK/A=";
   "https://github.com/sevanspowell/hw-aeson"."b5ef03a7d7443fcd6217ed88c335f0c411a05408" = "1dwx90wqavdl4d0npbzbxyh2pzi9zs1qz7nvsrb3n1cm2xbv4i5z";
-  "https://github.com/james-iohk/cardano-node"."b46e2a7b1cdb09c7bbf1471ea5cb349fac75fb40" = "0ppw3y5r41i40rgjshlnc90xv9i5x7rrr7y4k7nzhlahp5wp1h1z";
-  "https://github.com/james-iohk/cardano-node"."2b6978583b306ad28f7e1226b5d1ad604919c98a" = "06nggf0kpdh3whn6kqpw8s0484xw1q9r964rv125grbmpdx86dah";
+  "https://github.com/james-iohk/cardano-node"."34746b6c3dd524f99c10e51dd0787012081057a9" = "0k3p4csfb0xrfa99m0jydqa4bk1cfk22ypmy5hnz7lgjzp9i73dc";
+  "https://github.com/zliu41/cardano-node"."8cda3519bc3adc52b5354262e97250b6edcfc443" = "sha256-waQHNFYrvN0SdXGcG5L7j99g1cwnDL6W2QkmB5q8NqQ=";
 }
